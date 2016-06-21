32257d4d2bc1cf20f834bd65476722181c885b11
Calieston <calieston@hotmail.com>
2016-06-21 22:21:47 +0200

    Refactored module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 var controller = require('./app/controller');
 
@@ -9,14 +10,22 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
+  let pckg = fs.readFileSync('package.json');
+  pckg = JSON.parse(pckg);
+
   let city = 'Stuttgart'
   let urlStart='http://api.openweathermap.org/data/2.5/weather?q='
   let urlEnd='&lang=de&units=metric&APPID=384c499db848052e6aeed5df1388d5e7'
+
+  let widget = {};
+  widget._id = '1234567890';
+  widget.size = pckg.smartmirror.size[0];
+
   controller.get({city: city, urlStart: urlStart, urlEnd: urlEnd })
-  .then((weatherData) => {
-    res.render('app/view.jade', {
-      title: 'Weather Information',
-      weatherData: weatherData
+  .then((data) => {
+    res.render('./index.jade', {
+      data: data,
+      widget: widget,
     });
   });
 });
